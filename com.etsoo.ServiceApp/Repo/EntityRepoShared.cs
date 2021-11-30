262273ebc03ff9ff6a81b36273e66133ec79896c
Garry Xiao <xz@etsoo.com>
2021-12-01 11:11:47 +1300

    Add shared repos

---
+++ com.etsoo.ServiceApp/Repo/EntityRepoShared.cs
@@ -0,0 +1,32 @@
+﻿using com.etsoo.CoreFramework.Repositories;
+using com.etsoo.CoreFramework.User;
+using com.etsoo.ServiceApp.Application;
+using Microsoft.Data.SqlClient;
+
+namespace com.etsoo.ServiceApp.Repo
+{
+    /// <summary>
+    /// Shared entity repository
+    /// 共享实体仓库
+    /// </summary>
+    public abstract class EntityRepoShared<T> : EntityRepo<SqlConnection, T> where T : struct
+    {
+        /// <summary>
+        /// Override App, change its type
+        /// 重写App，修改类型
+        /// </summary>
+        protected override IServiceApp App { get; }
+
+        /// <summary>
+        /// Constructor
+        /// 构造函数
+        /// </summary>
+        /// <param name="app">Application</param>
+        /// <param name="flag">Flag</param>
+        protected EntityRepoShared(IServiceApp app, string flag, IServiceUser? user)
+            : base(app, flag, user)
+        {
+            App = app;
+        }
+    }
+}
---
+++ com.etsoo.ServiceApp/Repo/RepoShared.cs
@@ -0,0 +1,33 @@
+﻿using com.etsoo.CoreFramework.Repositories;
+using com.etsoo.CoreFramework.User;
+using com.etsoo.ServiceApp.Application;
+using Microsoft.Data.SqlClient;
+
+namespace com.etsoo.ServiceApp.Repo
+{
+    /// <summary>
+    /// Shared repository
+    /// 共享仓库
+    /// </summary>
+    public abstract class RepoShared : RepoBase<SqlConnection>
+    {
+        /// <summary>
+        /// Override App, change its type
+        /// 重写App，修改类型
+        /// </summary>
+        protected override IServiceApp App { get; }
+
+        /// <summary>
+        /// Constructor
+        /// 构造函数
+        /// </summary>
+        /// <param name="app">Application</param>
+        /// <param name="flag">Flag</param>
+        /// <param name="user">Current user</param>
+        protected RepoShared(IServiceApp app, string flag, IServiceUser? user = null)
+            : base(app, flag, user)
+        {
+            App = app;
+        }
+    }
+}
