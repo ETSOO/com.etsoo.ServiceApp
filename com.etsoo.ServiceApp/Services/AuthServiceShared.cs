e4d9c4a1f9fafb683635fd8eb6c9c0bf636b52ff
Garry Xiao <xz@etsoo.com>
2021-12-28 22:52:59 +1300

    Fix DeviceId lost when creating ServiceUser

--- com.etsoo.ServiceApp/Services/AuthServiceShared.cs
+++ com.etsoo.ServiceApp/Services/AuthServiceShared.cs
@@ -69,6 +69,9 @@ namespace com.etsoo.ServiceApp.Services
 
                 if (result.Ok)
                 {
+                    // Copy data
+                    result.Data["DeviceId"] = coreUser.DeviceId;
+
                     var serviceUser = ServiceUser.Create(result.Data, ip, coreUser.Language, coreUser.Region);
                     if (serviceUser == null)
                     {
