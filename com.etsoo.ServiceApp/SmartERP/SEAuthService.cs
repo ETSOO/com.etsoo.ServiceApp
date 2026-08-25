bdd5c5691710b2d8ce660199850ef27ac48bb378
Garry Xiao <xz@etsoo.com>
2026-08-25 17:47:56 +0800

    Add SEAuthServiceExtensions

--- com.etsoo.ServiceApp/SmartERP/SEAuthService.cs
+++ com.etsoo.ServiceApp/SmartERP/SEAuthService.cs
@@ -2,6 +2,7 @@
 using com.etsoo.CoreFramework.User;
 using com.etsoo.ServiceApp.Application;
 using com.etsoo.ServiceApp.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Npgsql;
 
@@ -23,6 +24,31 @@ namespace com.etsoo.ServiceApp.SmartERP
             IAuthService authService
         ) : base(app, userAccessor, logger, clientFactory, authService)
         {
+        }
+    }
+
+    public static class SEAuthServiceExtensions
+    {
+        /// <summary>
+        /// Add SmartERP Service Application authorization service
+        /// </summary>
+        /// <typeparam name="A">Generic applicaton type</typeparam>
+        /// <typeparam name="C">Generic configuration type</typeparam>
+        /// <param name="services">Service collection</param>
+        /// <returns>Service collection</returns>
+        public static IServiceCollection AddSEAuthService<A, C>(this IServiceCollection services)
+            where A : ISEServiceApp<C>
+            where C : ServiceAppConfiguration
+        {
+            services.AddScoped<ISEAuthService>((sp) => new SEAuthService<C>(
+                sp.GetRequiredService<A>(),
+                sp.GetRequiredService<CurrentUserAccessor>(),
+                sp.GetRequiredService<ILogger<SEAuthService<C>>>(),
+                sp.GetRequiredService<IHttpClientFactory>(),
+                sp.GetRequiredService<IAuthService>()
+            ));
+
+            return services;
         }
     }
 }
